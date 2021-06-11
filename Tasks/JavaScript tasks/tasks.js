dc4900df38dfca0d2ceabd59155b338d01aea6e1
gullu-babashli <gullu.babashli@gmail.com>
2021-06-11 19:54:39 +0400

    week09 tasks uploaded

---
+++ Tasks/JavaScript tasks/tasks.js
@@ -0,0 +1,25 @@
+adlar=['Ehmed','Ismayil','Sebine','Subhan','Mirnadir','Sabir']
+
+//1.
+// console.log(adlar.length);
+
+//2.
+    // for(i=0;i<adlar.length;i++){
+    //     console.log(adlar[i].length)
+    // }
+
+//3.
+    // for(i=0;i<adlar.length;i++){
+    //     console.log(adlar[i].includes("a"));
+    // }
+
+
+
+
+//6.
+    // function boyukHerf(){
+    //     for(i=0;i<adlar.length;i++){
+    //         console.log(adlar[i].toUpperCase())
+    //     }
+    // }
+    // boyukHerf()

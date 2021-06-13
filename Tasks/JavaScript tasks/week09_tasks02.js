8f929a1e968f9897c53a9701878403671724a972
gullu-babashli <gullu.babashli@gmail.com>
2021-06-13 22:03:40 +0400

    readme update

--- Tasks/JavaScript tasks/week09_tasks02.js
+++ Tasks/JavaScript tasks/week09_tasks02.js
@@ -3,17 +3,17 @@ adlar=['Ehmed','Ismayil','Sebine','Subhan','Mirnadir','Sabir']
 //1.
 // console.log(adlar.length);
 
+
 //2.
     // for(i=0;i<adlar.length;i++){
     //     console.log(adlar[i].length)
     // }
 
+
 //3.
     // for(i=0;i<adlar.length;i++){
     //     console.log(adlar[i].includes("a"));
     // }
-
-
 
 
 //6.
--- readme.md
+++ readme.md
@@ -2,3 +2,5 @@
 
 
 Website projects and tasks to be carried out within a 120-hour foundation program.
+
+[Main Project](https://www.templatemonsterpreview.com/demo/86751.html)
